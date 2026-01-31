c7e718fc47be3f3b9f1be862b40019f44f7ac2aa
Ezra-K <skoerkel6214@stu.d214.org>
2026-01-31 15:38:21 -0600

    Update Intake.java

--- src/main/java/org/wildstang/sample/subsystems/Intake.java
+++ src/main/java/org/wildstang/sample/subsystems/Intake.java
@@ -31,67 +31,38 @@ public class Intake implements Subsystem{
     private WsTalon intakeMotor, deployMotor;
 
     private enum DeployState {IN, OUT, STOWED};
-    private enum IntakeState {INTAKING, NEUTRAL, REVERSE};
+    private enum IntakeState {INTAKING, NEUTRAL, REVERSE, SLOW};
     private DeployState deploy = DeployState.IN;
     private IntakeState direction = IntakeState.NEUTRAL;
-
-    
-   /*  private boolean forward;
-    private boolean backwards;
-    private boolean in;
-    private boolean out;
-    private boolean stowed; */
 
 
     @Override
     public void inputUpdate(Input source) {
         if(Math.abs(rightTrigger.getValue()) > 0 || rightShoulder.getValue()){
-            //forward = true;
-            //backwards = false;
             direction = IntakeState.INTAKING;
         }
-        /* else{
-            forward = false;
-            backwards = false;
-        } */
        //I think we'll eventually want this to be running slower than the above intake, something like 0.25
        //So this will be a fourth IntakeState
         else if((Math.abs(leftTrigger.getValue()) > 0 || Math.abs(operatorLeftTrigger.getValue()) > 0 || 
                 operatorLeftShoulder.getValue() || operatorRightShoulder.getValue()) && (goodToFire())){
-            // forward = true;
-            // backwards = false;
-            direction = IntakeState.INTAKING;
+            direction = IntakeState.SLOW;
         }
-                
-          /*   else{forward=false; backwards = false;} */
         else if(operatorB.getValue()){
-           // backwards = true;
-           // forward = false;
            //I think you want REVERSE below
-           direction = IntakeState.INTAKING;
+           direction = IntakeState.REVERSE;
         }  
         else{
             direction = IntakeState.NEUTRAL;
         }
-       // else{forward = false;backwards = false;}
         
        if(operatorX.getValue()){
-            //in = true;
-            //out = false;
-            //stowed = false;
             deploy = DeployState.IN;
         }  
      
         if(Math.abs(rightTrigger.getValue()) > 0 || rightShoulder.getValue()){
-            //out = true;
-            //in = false;
-            //stowed = false;
             deploy = DeployState.OUT;
         }
         else if(Math.abs(leftTrigger.getValue()) > 0){
-            //stowed = true;
-            //in = false;
-            //out = false;
             deploy = DeployState.STOWED;
         }
 
@@ -102,6 +73,9 @@ public class Intake implements Subsystem{
         intakeMotor = (WsTalon) WsOutputs.INTAKE.get();
         deployMotor = (WsTalon) WsOutputs.INTAKE_DEPLOY.get();
         //we'll need to do some motor initializing here, intake_deploy PID and current limits for both of them
+    deployMotor.initClosedLoop(1,0,0);
+        intakeMotor.setCurrentLimit(120,70);
+        deployMotor.setCurrentLimit(40,40);
 
         rightTrigger = (WsJoystickAxis) Core.getInputManager().getInput(WsInputs.DRIVER_RIGHT_TRIGGER);
         rightTrigger.addInputListener(this);
@@ -137,6 +111,9 @@ public class Intake implements Subsystem{
         }
         else if (direction == IntakeState.REVERSE){
             intakeMotor.setSpeed(-1.0);
+        }
+        else if (direction == IntakeState.SLOW){
+            intakeMotor.setSpeed(0.25);
         }
         else{
            intakeMotor.setSpeed(0.0);
