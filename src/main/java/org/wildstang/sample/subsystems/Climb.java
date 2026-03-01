1a203b851a26b3378cd18fda966b3dc458b1acc8
Sab3rson <porebawheeling@gmail.com>
2026-03-01 09:18:40 -0600

    Copied over Climb code over to this branch

--- src/main/java/org/wildstang/sample/subsystems/Climb.java
+++ src/main/java/org/wildstang/sample/subsystems/Climb.java
@@ -2,21 +2,44 @@ package org.wildstang.sample.subsystems;
 
 import org.wildstang.framework.io.inputs.Input;
 import org.wildstang.framework.subsystems.Subsystem;
+import org.wildstang.hardware.roborio.inputs.WsDPadButton;
 import org.wildstang.hardware.roborio.outputs.WsTalon;
+import org.wildstang.sample.robot.WsInputs;
 import org.wildstang.sample.robot.WsOutputs;
 
 public class Climb implements Subsystem{
 
-    private WsTalon climb1Motor, climb2Motor;
+   private WsTalon climb1Motor;
+    private WsDPadButton dpadUp, dpadDown;
+    private enum ClimbState {UP, DOWN};
+    private ClimbState climbState = ClimbState.DOWN;
+    private double climbUpSpeed = 1;
+    private double climbDownSpeed = -1;//probably should be -1 for now
+    
     
     @Override
     public void inputUpdate(Input source) {
+        if(dpadUp.getValue()){
+            climbState = ClimbState.UP;
+        }
+         if(dpadDown.getValue()){
+            climbState = ClimbState.DOWN;
+        }
     }
 
     @Override
     public void init() {
         climb1Motor = (WsTalon) WsOutputs.CLIMB1.get();
-        // climb2Motor = (WsTalon) WsOutputs.CLIMB2.get();
+        climb1Motor.setBrake();
+        climb1Motor.initClosedLoop(12, 0, 0);
+        climb1Motor.setCurrentLimit(20,70);
+
+        dpadUp = (WsDPadButton) WsInputs.DRIVER_DPAD_UP.get();
+        dpadUp.addInputListener(null);
+        dpadDown = (WsDPadButton) WsInputs.DRIVER_DPAD_DOWN.get();
+        dpadDown.addInputListener(null);
+
+        
     }
 
     @Override
@@ -25,6 +48,15 @@ public class Climb implements Subsystem{
 
     @Override
     public void update() {
+        if(climbState == ClimbState.UP){
+            //this works for testing
+            //we'll eventually change this to use .setPosition(target),
+            //with an upper and lower target instead of manual
+            climb1Motor.setSpeed(climbUpSpeed);
+        }
+        else{
+            climb1Motor.setSpeed(climbDownSpeed);
+        }
     }
 
     @Override
@@ -35,9 +67,14 @@ public class Climb implements Subsystem{
     public void initSubsystems() {
     }
 
+    public void setClimbState(ClimbState state){
+        climbState = state;
+    }
+
     @Override
     public String getName() {
         return "Climb";
     }
+    
     
 }
